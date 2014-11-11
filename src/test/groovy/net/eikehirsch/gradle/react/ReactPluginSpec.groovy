df1b0e564e885fd5ad629b9f38347c8b3deb2ee0
Eike Hirsch <eike.hirsch@smartsteuer.de>
2014-11-11 10:21:03 +0100

    Rewrote the pluing test.

--- src/test/groovy/net/eikehirsch/gradle/react/ReactPluginSpec.groovy
+++ src/test/groovy/net/eikehirsch/gradle/react/ReactPluginSpec.groovy
@@ -1,12 +1,25 @@
 package net.eikehirsch.gradle.react
 
+import com.moowork.gradle.node.NodePlugin
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 
 class ReactPluginSpec extends Specification {
 
-  def "apply the plugin"() {
+  def "applying the plugin adds react tasks to project"() {
+    given:
+    def project = ProjectBuilder.builder().build()
+
+    when:
+    project.apply plugin: 'net.eikehirsch.react'
+
+    then:
+    project.tasks.installReact instanceof ReactInstallTask
+    project.tasks.jsx instanceof JSXTask
+  }
+
+  def "applying the plugin adds jsxTask to global namespace"() {
     given:
     def project = ProjectBuilder.builder().build()
 
@@ -16,4 +29,16 @@ class ReactPluginSpec extends Specification {
     then:
     project.JSXTask == JSXTask
   }
+
+  def "applying the plugin applies the node plugin to to the project"() {
+    given:
+    def project = ProjectBuilder.builder().build()
+
+    when:
+    project.apply plugin: 'net.eikehirsch.react'
+
+    then:
+    project.plugins.hasPlugin(NodePlugin.class)
+  }
+
 }
