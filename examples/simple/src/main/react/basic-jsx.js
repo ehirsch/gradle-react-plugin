761beaf9c15ec6a681b57a90f0cf23d9573900cb
Eike Hirsch <eike.hirsch@smartsteuer.de>
2014-11-13 16:21:22 +0100

    removed a comment

--- examples/simple/src/main/react/basic-jsx.js
+++ examples/simple/src/main/react/basic-jsx.js
@@ -18,26 +18,3 @@ setInterval(function() {
 			document.getElementById('container')
 	);
 }, 50);
-
-/*
- / **
- * @jsx React.DOM
- * /
-var ExampleApplication = React.createClass({
-	render() {
-		var elapsed = Math.round(this.props.elapsed  / 100);
-		var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-		var message =
-				`React has been successfully running for ${seconds} seconds.`;
-
-		return <p>{message}</p>;
-	}
-});
-var start = new Date().getTime();
-setInterval(() => {
-	React.renderComponent(
-			<ExampleApplication elapsed={new Date().getTime() - start} />,
-			document.getElementById('container')
-	);
-}, 50);
- */
